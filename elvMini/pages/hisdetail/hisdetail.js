66895bae3d451d921b9edfcf389baa991cb42974
LiuZIXUAN <2983080618@qq.com>
2024-04-20 17:37:44 +0800

    update function:history

--- elvMini/pages/hisdetail/hisdetail.js
+++ elvMini/pages/hisdetail/hisdetail.js
@@ -11,6 +11,10 @@ Page({
     safer:[],
     profile:[],
     parts:[],
+    parts_beforelist:[],
+    parts_afterlist:[],
+    parts_before:0,
+    parts_after:0,
   },
 
   /**
@@ -42,10 +46,24 @@ Page({
     console.log({partsRecord});
     get("/parts/record/index",{belongto,id: partsRecord}).then(({ data: { list } }) => {
       console.log({list});
-      this.setData({ parts:list });
+            this.setData({ parts:list ,parts_before:list[0].part_id,
+              parts_after:list[0].replace_part});
+      get("/parts/partsprofile/index",{belongto,id: this.data.parts_before}).then(({ data: { list } }) => {
+        console.log({list});
+        this.setData({ parts_beforelist:list });
+      }).catch(error => { 
+        this.showErrorToast(error);
+      }); 
+      get("/parts/partsprofile/index",{belongto,id: this.data.parts_after}).then(({ data: { list } }) => {
+        console.log({list});
+        this.setData({ parts_afterlist:list });
+      }).catch(error => { 
+        this.showErrorToast(error);
+      }); 
     }).catch(error => { 
       this.showErrorToast(error);
     }); 
+    console.log(this.data.parts_before);
   },
 
   /**
