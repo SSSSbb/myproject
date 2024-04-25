ddd4965bf88088c05eaf74239b9a4887b9eace0d
LiuZIXUAN <2983080618@qq.com>
2024-04-25 18:39:25 +0800

    add schedule -- mini

--- elvMini/pages/view/view.js
+++ elvMini/pages/view/view.js
@@ -1,18 +1,50 @@
 // pages/view/view.js
+const { get } = require("../../api/api");
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    schedulelist: [], 
+    userlist:[],
+    scheduleTable: [[], [], [], [], []], // 初始化排班表数据
+    currentUser:"",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    const { user } = app.globalData;
+    const { belongto, username } = user;
+    this.setData({ currentUser:username });
+    get("/schedule/schedulelist/index",{belongto}).then((res) => {
+      this.setData({ schedulelist:res.data });
+      get("/rbac/user/index",{belongto}).then(({ data: { list } }) => {
+        console.log({list});
+        this.setData({ userlist:list });
+        const userIdToUsernameMap = {};
+        this.data.userlist.forEach(user => {
+                 userIdToUsernameMap[user.id] = user.username;
+        });
+             this.data.schedulelist.forEach(item => {
+              item.username = userIdToUsernameMap[item.userid]; 
+            });
+            const scheduletable = [[], [], [], [], []];
+            this.data.schedulelist.forEach(item => {
+              const { slot, weekday } = item;
+              scheduletable[slot][weekday] = item; 
+            });
+            this.setData({ scheduleTable: scheduletable });
+            console.log(this.data.scheduleTable);
+      }).catch(error => { 
+        this.showErrorToast(error);
+      }); 
+    }).catch(error => { 
+      console.log({error});
+    }); 
   },
 
   /**
@@ -42,7 +74,12 @@ Page({
   onUnload() {
 
   },
-
+  showErrorToast(error) {
+    wx.showToast({
+      icon: "none",
+      title: error || "未知错误",
+    });
+  },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
